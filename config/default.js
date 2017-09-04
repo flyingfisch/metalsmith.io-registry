ba9df6309bf943cb089e9a4c3a6e858b7d8b81ee
Levi Wheatcroft <levi@wheatcrofts.com.au>
2017-09-04 18:00:57 +0800

    git token instructions

--- config/default.js
+++ config/default.js
@@ -1,3 +1,11 @@
+// don't put your credentials in ./config/default.js, copy that file to
+// ./config/local.js which is .gitignored, and edit it there.
+//
+// if you have 2 factor auth on github then user & pass won't work for you, use
+// the token instead.
+//
+// to create a token go to https://github.com/settings/tokens and generate one
+// you don't need to enable any scopes
 module.exports = {
   // uncomment below if you're using 2 factor
   // 'github-auth': {
