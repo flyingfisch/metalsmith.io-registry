fcd9cb6cd79378eef3e4ba23437a0f5b0d93f6ec
Levi Wheatcroft <levi@wheatcrofts.com.au>
2017-09-04 17:55:18 +0800

    added token github auth

--- config/default.js
+++ config/default.js
@@ -1,6 +1,12 @@
 module.exports = {
-  githubCredentials: {
-    username: 'myusername',
-    password: 'mypassword'
-  }
+  // uncomment below if you're using 2 factor
+  // 'github-auth': {
+  //   token: 'mytoken'
+  // }
+
+  // uncomment this and set user & pass if you're not using 2 factor
+  // 'github-auth': {
+  //   username: 'myusername',
+  //   password: 'mypassword'
+  // }
 }
